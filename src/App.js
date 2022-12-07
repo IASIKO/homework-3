91affb1b8ab3cf9587448ed026e16bedc8a97961
IASIKO <iaseshviligi@gmail.com>
2022-12-07 23:12:02 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import { useState, useEffect } from "react";
+import "./App.css";
 
-import './App.css';
+const generateUsers = () => {
+  return [
+    "name1",
+    "name2",
+    "name3",
+    "name4",
+    "name5",
+    "name6",
+    "name7",
+    "name8",
+    "name9",
+    "name10",
+  ];
+};
 
 function App() {
-  return
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    setState(generateUsers());
+  }, []);
+
+  useEffect(() => {
+    document.title = state.length + " users left";
+  }, [state]);
+
+  const clickHandler = () => {
+    const randomValue = state[Math.floor(Math.random() * state.length)];
+    setState(state.filter((element) => element !== randomValue));
+  };
+
+  return (
+    <>
+      <ul>
+        {state.map((number) => (
+          <li key={number}>{number}</li>
+        ))}
+      </ul>
+      <button onClick={clickHandler}>HIDE</button>
+    </>
+  );
 }
 
 export default App;
